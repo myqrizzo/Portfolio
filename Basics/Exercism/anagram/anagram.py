ef99d5e88272c05217a3db447213675990cc3f6e
Michael Rizzo <mdrizzzoh@gmail.com>
2020-06-27 02:43:14 -0400

    Delete anagram.py

--- Basics/Exercism/anagram/anagram.py
+++
@@ -1,4 +0,0 @@
-def find_anagrams(word, candidates):
-    return [candidate for candidate in candidates
-            if sorted(word.lower()) == sorted(candidate.lower())
-            and word.lower() != candidate.lower()]
